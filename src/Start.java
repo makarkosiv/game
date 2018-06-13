31ec557477a15c43e00b984cf5449825c6b6a1be
Makar Barzikov <makarkosiv@gmail.com>
2018-06-13 15:46:48 +0300

    fist commit

---
+++ src/Start.java
@@ -0,0 +1,102 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Start extends Applet implements Runnable, KeyListener {
+    @Override
+    public void init() {
+        setSize(800, 480);
+        setBackground(Color.BLACK);
+        setFocusable(true);
+        addKeyListener(this);
+        Frame frame = (Frame) this.getParent().getParent();
+        frame.setTitle("Javist");    }
+
+    @Override
+    public void start() {
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            repaint();
+            try {
+                Thread.sleep(17);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                System.out.println("Move up");
+                break;
+
+            case KeyEvent.VK_DOWN:
+                System.out.println("Move down");
+                break;
+
+            case KeyEvent.VK_LEFT:
+                System.out.println("Move left");
+                break;
+
+            case KeyEvent.VK_RIGHT:
+                System.out.println("Move right");
+                break;
+
+            case KeyEvent.VK_SPACE:
+                System.out.println("Jump");
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                System.out.println("Stop moving up");
+                break;
+
+            case KeyEvent.VK_DOWN:
+                System.out.println("Stop moving down");
+                break;
+
+            case KeyEvent.VK_LEFT:
+                System.out.println("Stop moving left");
+                break;
+
+            case KeyEvent.VK_RIGHT:
+                System.out.println("Stop moving right");
+                break;
+
+            case KeyEvent.VK_SPACE:
+                System.out.println("Stop jumping");
+                break;
+
+        }
+
+    }
+}
